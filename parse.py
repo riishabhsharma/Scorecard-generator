252827e0bf173809c26f98a8312899381e22512d
riishabhsharma <rishabhsharma07612@gmail.com>
2025-05-03 22:01:37 +0530

    Added Evaulation Method

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import json
 
 # Load the file
 file_path = r"C:\Learn\Projects\sources\DL01129290_2083O24353S16D61488E1.html"
@@ -9,7 +10,7 @@ with open(file_path, "r", encoding="utf-8") as file:
 soup = BeautifulSoup(html, "html.parser")
 
 # Prepare a list to hold extracted data
-extracted_data = []
+extracted_data = {}
 
 # Find all question panels
 question_panels = soup.find_all("div", class_="question-pnl")
@@ -28,9 +29,36 @@ for panel in question_panels:
             option_id_tag = panel.find("td", string=f"Option {chosen_option} ID :")
             if option_id_tag:
                 answer_id = option_id_tag.find_next_sibling("td").text.strip()
-                extracted_data.append((question_id, answer_id))
+                extracted_data[question_id]=answer_id
 
 extracted_data
-print(extracted_data)
-print(len(extracted_data))
-print(type(extracted_data))
+# print(extracted_data)
+# print(len(extracted_data))
+# print(type(extracted_data))
+
+with open("answerkey.json", "r") as file:
+    answer_key=json.load(file)
+marks=0
+correct_answer=0
+wrong_answer=0
+total_attempt=0
+for key in answer_key:
+    if key not in extracted_data:
+        continue
+    user_value=extracted_data[key]
+    if(user_value==answer_key[key]):
+        print(f"{key}: correct")
+        marks+=4
+        correct_answer+=1
+        total_attempt+=1
+
+    else:
+        print(f"{key}:wrong (your answer {user_value}, correct:{answer_key[key]})")
+        marks-=1
+        wrong_answer+=1
+        total_attempt+=1
+
+print(marks)
+print(correct_answer)
+print(wrong_answer)
+print(total_attempt)
